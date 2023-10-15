da1972ca8f2a9537a78bb113e0db0bfe3470b9fd
Mikołaj Skuczeń <117775670+mikson23@users.noreply.github.com>
2023-10-15 23:14:13 +0200

    Add files via upload

---
+++ spec03_ochrona/app/calc.php
@@ -0,0 +1,87 @@
+<?php
+
+require_once dirname(__FILE__).'/../config.php';
+
+include _ROOT_PATH.'/app/security/check.php';
+
+function getParams(&$x,&$y,&$o){
+	$x = isset($_REQUEST['x']) ? $_REQUEST['x'] : null;
+	$y = isset($_REQUEST['y']) ? $_REQUEST['y'] : null;
+	$o = isset($_REQUEST['o']) ? $_REQUEST['o'] : null;	
+}
+
+
+function validate(&$x, &$y, &$o, &$messages){
+	if ( ! (isset($x) && isset($y) && isset($o) )) {
+
+	return false;
+	}
+
+
+
+
+	if ( $x == "") {
+		$messages [] = 'Nie podano kwoty';
+	}
+	if ( $o == "") {
+		$messages [] = 'Nie podano oprocentowania kredytu (RRSO)';
+	}
+	if ( $y == "") {
+		$messages [] = 'Nie podano ilości rat / na ile miesięcy';
+	}
+	
+
+	if (empty( $messages )) {
+		
+		
+		if (! is_numeric( $x )) {
+			$messages [] = 'Kwota kredytu nie jest prawidłowa';
+		}
+		
+		if (! is_numeric( $y )) {
+			$messages [] = 'Ilość miesięcy nie jest prawidłowa';
+		}	
+		if (! is_numeric( $o )) {
+			$messages [] = 'Wartość oprocentowania jest nieprawidłowa';
+		}
+		//	uzależnienie liczenia raty od roli użytkownika
+		if ( $_SESSION['role'] == "user" && $o < 10) {
+				$messages [] = 'Tylko administrator może brać kredyty z RRSO mniejszym niż 10%';
+			}
+
+	}
+
+	if (count ( $messages ) != 0) return false;
+	else return true;
+}
+
+function process(&$x,&$y,&$o,&$messages,&$result,&$rata){
+	if (empty ( $messages )) { 
+		
+		
+		$x = intval($x);
+		$y = intval($y);
+		$o = floatval($o);
+		
+		
+		$result = $x + $x*($y/12*($o/100));
+		$rata = $result/$y;
+		$rata = round($rata, 2);
+		$result = round($result, 2);
+	}
+}
+
+$x = null;
+$y = null;
+$o = null;
+$result = null;
+$rata = null;
+$messages = array();
+
+//pobierz parametry i wykonaj zadanie jeśli wszystko w porządku
+getParams($x,$y,$o);
+if ( validate($x,$y,$o,$messages) ) { // gdy brak błędów
+	process($x,$y,$o,$messages,$result,$rata);
+}
+
+include 'calc_view.php';
---
+++ spec03_ochrona/app/calc_view.php
@@ -0,0 +1,60 @@
+
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl"  data-bs-theme="dark">
+ 
+<head>
+	<meta charset="utf-8" />
+	<title>Kalkulator kredytów</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+	<div class="row">
+	<div class="col-4"></div>
+
+	<div class="card text-center col-4">
+	<form action="<?php print(_APP_URL);?>/app/calc.php" method="post">
+		<div class="form-floating mb-3">
+			<input type="number" class="form-control" id="id_x" name="x" placeholder="1" value="<?php out($x); ?>">
+			<label for="id_x">Kwota kredytu:</label>
+		</div>
+		<div class="form-floating mb-3">
+			<input type="text" class="form-control" id="id_o" name="o" placeholder="1" value="<?php out($o); ?>">
+			<label for="id_o">Oprocentowanie:</label>
+		</div>
+		<div class="form-floating mb-3">
+			<input type="number" class="form-control" id="id_y" name="y" placeholder="1" value="<?php out($y); ?>">
+			<label for="id_y">Na ile miesięcy:</label>
+		</div>
+		
+		
+		<input type="submit" class="btn btn-outline-light" value="Oblicz" />
+		<a href="<?php print(_APP_ROOT); ?>/app/security/logout.php" class="btn btn-outline-warning">Wyloguj</a>
+	</form>	
+
+<?php
+//wyświeltenie listy błędów, jeśli istnieją
+if (isset($messages)) {
+	if (count ( $messages ) > 0) {
+		echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px;" class="bg-danger">';
+		foreach ( $messages as $key => $msg ) {
+			echo '<li>'.$msg.'</li>';
+		}
+		echo '</ol>';
+	}
+}
+?>
+<div>
+<?php if (isset($result) && isset($rata)){ ?>
+<div style="margin: 20px; padding: 10px; border-radius: 5px;  " class='bg-success'>
+<?php echo 'Koszt pożyczki: '.$result; ?>zł<br>
+<?php echo 'Rata miesięczna: '.$rata; ?>zł<br>
+</div>
+<?php } ?>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</div> 
+
+<div class="col-4"></div>
+
+</div></body>
+</html>
