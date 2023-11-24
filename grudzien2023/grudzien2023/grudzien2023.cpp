0ea6c5e63f0228d32466de11af786e04e806ef5e
Mikołaj Skuczeń <117775670+mikson23@users.noreply.github.com>
2023-11-24 09:22:21 +0100

    Add files via upload

---
+++ grudzien2023/grudzien2023/grudzien2023.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+
+using namespace std;
+const int N = 2000;
+
+
+int stos(int a, int b) {
+    
+    if (a > b)
+    {
+        return 0;
+    }
+    if (2 * a == b) {
+        return 1;
+    }else if (2*a+1 == b)
+    {
+        return 1;
+    }else if(stos(2 * a, b) == 1)
+    {
+         return stos(2 * a, b);
+    }else
+    {
+        return stos(2 * a + 1, b);
+    }
+    
+}
+
+
+
+int main()
+{
+    ifstream we("pary.txt");
+    int z = 0;
+    
+    int A[N] = {};
+    while (!we.eof()) {
+        we >> A[z];
+        z++;
+    }
+    we.close();
+    
+    for (int i = 0; i < 1999; i = i + 2)
+    {
+        if (stos(A[i], A[i+1]) == 1)
+        {
+            cout << A[i] << " " << A[i + 1] << endl;
+        }
+    }
+
+    
+}
